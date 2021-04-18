97121140bc5825668cb0369514ec5806e977b7a8
Martin Elff <martin@elff.eu>
2021-04-18 22:22:14 +0200

    Add support for time series objects

--- R/repr_star_pre.R
+++ R/repr_star_pre.R
@@ -1,8 +1,10 @@
-repr_html_pre <- function(obj, ...)
-    paste0('<pre>\n',
-	   paste(capture.output(print(obj)),
+repr_html_pre <- function(obj, FUN=print)
+    paste0('\n<pre>\n',
+	   paste(capture.output(FUN(obj)),
 		 collapse='\n'),
-	   '</pre>')
+	   '\n</pre>\n')
+
+
 repr_latex_pre <- function(obj, FUN=print)
     paste0('\n\\begin{Verbatim}[commandchars=\\\\\\{\\}]\n',
 	   paste(capture.output(FUN(obj)),
